359821573ca0e09120619e9f3f69ed56884e09b2
Maksym Bahmet <bagmet.maxim@gmail.com>
2023-05-25 05:41:53 +0200

    Add javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"application"},{"l":"controller"},{"l":"model"},{"l":"model.cell"},{"l":"model.cell.edible"},{"l":"view"},{"l":"view.components"}];updateSearchResults();
